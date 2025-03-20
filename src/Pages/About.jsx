9bfc45cdef9aa3020757385dc753c3c20de24243
ABOI SAMSON ABOI <samsonaboi10@gmail.com>
2025-03-20 13:39:38 +0100

    text add

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="contact">
+      <div className="contact-col">
+        <h3>Send us a Message</h3>
+        <p>Feel free to reach out through contact form or find our contact information below. your feedback, questions,
+        and suggestions are important to us as we strive to provide exceptional service to our client.  </p>
+      </div>
+    </div>
   )
 }
 
