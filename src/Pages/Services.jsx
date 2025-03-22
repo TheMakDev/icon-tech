27aeb4ca49fd3c92d0b5bfe4eddfb4a4a3514901
ABOI SAMSON ABOI <samsonaboi10@gmail.com>
2025-03-22 11:24:00 +0100

    update

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Styles/Services.css'
+import './Services.css'
 
 
 const Services = () => {
@@ -10,8 +10,9 @@ const Services = () => {
       </div>
       <div className="row">
         <div className="service-box-items">
-          <div className="icon"> <img src={} alt="" />
-            <h3>STAY CONNECTED</h3>
+          <div className="icon"> 
+            
+            <h3>Stay Connected</h3>
             <p>Our automation systems ensure uninterrupted connectivity, no matter your 
               location. Stay in the loop, never missing a 
               moment. Our tool kit includes:
@@ -22,6 +23,48 @@ const Services = () => {
               <li>Fibre-to-the-Home Internet Connection</li>
               <li>High Definition Pan tilt Zoom Cameras (PTZ)</li>
               <li>Motion Sensitive Lighting</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service-box-items">
+          <div className="icon"> 
+            <h3>Be Seen </h3>
+            <p>Our AV conferencing solutions enhance visuals for crystal-clear meetings, whether virtual or in-person
+            </p>
+            <ul>
+              <li>Video Walls</li>
+              <li>Video Conferencing</li>
+              <li>Repeater Screens</li>
+              <li>Projectors (PTZ)</li>
+              <li>Desk Pop-Up Monitors</li>
+              <li>Video Recording Systems</li>
+              <li>Digital Microphone Control System</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service-box-items">
+          <div className="icon"> 
+            <h3>Be Heard</h3>
+            <p>Amidst the noise, we make sure your voice is heard loud and clear. Every point matters. Our offerings encompass:
+            </p>
+            <ul>
+              <li>Conference Interpretation System</li>
+              <li>Room Audio Technology</li>
+              <li>Audio Recording System</li>
+              <li>Conferencing Voting Software</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service-box-items">
+          <div className="icon"> 
+            <h3>Take Control</h3>
+            <p>Security is paramount. Our advanced security equipment empowers you to oversee your environment effectively:
+            </p>
+            <ul>
+              <li>State-of-the-Art Security Surveillance Systems</li>
+              <li>Biometric & Remote Access Control Systems</li>
+              <li>Audio Recording System</li>
+              <li>Smart Security System and Control Panel</li>
             </ul>
           </div>
         </div>
