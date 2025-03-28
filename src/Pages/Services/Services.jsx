b3081c8b3d0a921559a5bf7b356f0bf12598c891
TheMakDev <makanjuolaenitan71@gmail.com>
2025-03-28 18:42:16 +0100

    new

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -6,14 +6,14 @@ import Service from '../../Component/Service/Service'
 const Services = () => {
   return (
     
-    <>
+    <section className='servivces-page'>
     <div className="service-hero">
       <div className="hero-text">
         <h1>Transforming Spaces with Seamless Technology Integration</h1>
       </div>
     </div>
     <Service />
-    </>
+    </section>
     
 
   
