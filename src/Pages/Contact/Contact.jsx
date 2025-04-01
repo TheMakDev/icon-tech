c8b5c31d19c55f89c8d93f2e410f25ed7a13e855
aboisam <samsonaboi10@gmail.com>
2025-04-01 11:14:43 +0100

    form addition

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -28,8 +28,20 @@ const Contact = () => {
           <p className="contact-details">Learn more about our job openings</p>
           <button className="contact-button">Explore Our Service</button>
         </div>
+        <div className="contact-col">
+        <form>
+          <label>Your name</label>
+            <input type="text" placeholder="Your Name" required />
+          <label>Phone Number</label>
+            <input type="text" placeholder="Phone Number" required/>
+          <label>Write your messages here</label>
+          <textarea name="message"rows='6'placeholder='Enter Your Message' required></textarea>
+          <button type='submit' className='btn dark-btn'>Submit now</button>
+        </form>
+      </div>
       </div>
     </div>
+
     <div className="contact-map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31520.086002441938!2d7.425991312114974!3d9.062783054346449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sSuite%20C7%2C%20Rukaya%20Plaza%2C%2093%20Obafemi%20Awolowo%20Way%2C%20Jabi%20District%2C%20Abuja.!5e0!3m2!1sen!2sng!4v1742824409982!5m2!1sen!2sng"
