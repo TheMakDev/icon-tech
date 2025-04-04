91e67a28e892b3d6607bb78c0e213fa6aacb649f
TheMakDev <makanjuolaenitan71@gmail.com>
2025-04-04 14:55:50 +0100

    new

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -8,12 +8,7 @@ import image1 from './image1.jpeg'
 import image4 from './image4.jpeg'
 import image2 from './image2.png'
 
-// // import logo from "./logo.png";
-// import menu_icon from "./menu_icon.svg";
-// import cross_icon from "./cross_icon.png";
-// import about_image2 from "./about_image 2.jpeg";
-// import about_image from "./about_image1.jpeg";
-// import video from "./bg.mp4";
+
 
 export const assets = {
   logo,
